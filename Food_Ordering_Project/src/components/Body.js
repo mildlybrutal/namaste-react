91331724b01ca5a0179c54f33ac9d3798b4c4581
mildlybrutal <singuakash23@lpu.in>
2024-06-10 20:39:26 +0530

    apiFetchError

--- Food_Ordering_Project/src/components/Body.js
+++ Food_Ordering_Project/src/components/Body.js
@@ -1,10 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../utils/mockData";
 const Body = () => {
 	//Local State Variable - Super Powerful Variable -> setListofRest = Update the list
-	let [listOfRestaurants, setListOfRestaraunt] = useState(resList);
+	let [listOfRestaurants, setListOfRestaraunt] = useState([]);
 	let [searchInput, setSearchInput] = useState("");
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		const data = await fetch(
+			"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+		);
+
+		const json = await data.json();
+		console.log(json);
+		//Optional Chaining
+		const restaurantCards = json?.data?.cards[2]?.data?.data?.cards;
+
+		setListOfRestaraunt(restaurantCards);
+	};
+
+	if (listOfRestaurants.length === 0) {
+		return <h1>Loading...</h1>;
+	}
+
 	return (
 		<div className="body">
 			<div className="search">
